53884ece3d77969d2897b890ae91dc7a49db6ef4
ricagmz <2015081531@ust-ics.mygbiz.com>
2018-09-05 14:14:06 +0000

    Initial commit

---
+++ app/src/main/java/com/gomez/rica/gomezricalab1/Lab2.java
@@ -0,0 +1,13 @@
+package com.gomez.rica.gomezricalab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lab2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab2);
+    }
+}
